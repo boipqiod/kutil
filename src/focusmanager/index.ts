9dcf958a4bbded8a361a3a06e557977ee16fcdce
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 17:29:17 +0900

    TEST. 푸시 테스트

--- src/focusmanager/index.ts
+++ src/focusmanager/index.ts
@@ -15,7 +15,6 @@ window.onload = async () =>{
                     // 사용자가 알림 권한을 허용한 경우
                     alert('알림 권한을 허용했습니다.');
                     await ServiceWorkerHelper.registerServiceWorker()
-
                 }
                 else{
                     // 사용자가 알림 권한을 거부한 경우
@@ -23,8 +22,11 @@ window.onload = async () =>{
                 }
             });
         });
+    }else   if (Notification.permission === 'granted') {
+        await ServiceWorkerHelper.registerServiceWorker()
     }
     new Controller().init()
+
 
     // await ServiceWorkerHelper.pushMessageToServiceWorker({type: 'init', data: {url: BASE_URL}})
 
