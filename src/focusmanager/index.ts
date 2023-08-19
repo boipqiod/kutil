f27af338c5bc72abe178a63803c7af14947469f2
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 17:00:59 +0900

    TEST. 푸시 테스트

--- src/focusmanager/index.ts
+++ src/focusmanager/index.ts
@@ -5,13 +5,17 @@ import swal from 'sweetalert2'
 
 window.onload = async () =>{
     // Indicator.instance.setIndicator()
+    console.log(Notification.permission)
+
     if (Notification.permission === 'default') {
         // 사용자가 아직 알림 권한을 선택하지 않았을 때
         document.getElementById('subscribeButton').addEventListener('click', function() {
-            Notification.requestPermission().then(function(permission) {
+            Notification.requestPermission().then(async function(permission) {
                 if (permission === 'granted') {
                     // 사용자가 알림 권한을 허용한 경우
                     alert('알림 권한을 허용했습니다.');
+                    await ServiceWorkerHelper.registerServiceWorker()
+
                 }
                 else{
                     // 사용자가 알림 권한을 거부한 경우
@@ -20,7 +24,6 @@ window.onload = async () =>{
             });
         });
     }
-    await ServiceWorkerHelper.registerServiceWorker()
     new Controller().init()
 
     // await ServiceWorkerHelper.pushMessageToServiceWorker({type: 'init', data: {url: BASE_URL}})
