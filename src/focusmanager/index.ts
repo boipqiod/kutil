6c9df5f32203cded2ca9a9d45fde903f848ec405
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 16:37:24 +0900

    TEST. 푸시 테스트

--- src/focusmanager/index.ts
+++ src/focusmanager/index.ts
@@ -5,9 +5,9 @@ import ServiceWorkerHelper from "../utils/ServiceWorkerHelper";
 window.onload = async () =>{
     Indicator.instance.setIndicator()
 
+    await ServiceWorkerHelper.registerServiceWorker()
     const notification = await Notification.requestPermission()
     console.log("notification", notification)
-    await ServiceWorkerHelper.registerServiceWorker()
     new Controller().init()
 
     // await ServiceWorkerHelper.pushMessageToServiceWorker({type: 'init', data: {url: BASE_URL}})
