9712d9ea74fe57033ac96947dc3cc7a37118780e
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 15:53:11 +0900

    TEST. 푸시 테스트

--- src/focusmanager/index.ts
+++ src/focusmanager/index.ts
@@ -8,7 +8,6 @@ window.onload = async () =>{
     const notification = await Notification.requestPermission()
     console.log("notification", notification)
     await ServiceWorkerHelper.registerServiceWorker()
-
     new Controller().init()
 
     // await ServiceWorkerHelper.pushMessageToServiceWorker({type: 'init', data: {url: BASE_URL}})
