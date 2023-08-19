9ea69521b932158f3f72dde5098b50bd826be553
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 16:53:09 +0900

    TEST. 푸시 테스트

--- src/focusmanager/index.ts
+++ src/focusmanager/index.ts
@@ -1,14 +1,21 @@
 import {Controller} from "./Controller";
 import {Indicator} from "../utils/Indicator";
 import ServiceWorkerHelper from "../utils/ServiceWorkerHelper";
+import swal from 'sweetalert2'
 
 window.onload = async () =>{
     Indicator.instance.setIndicator()
 
+    const push = await swal.fire({
+        title: '푸시 알림을 받으시겠습니까?',
+        text: '푸시 알림을 받으시면 새로운 글이 올라올 때 알림을 받을 수 있습니다.',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: '네',
+        cancelButtonText: '아니오'
+    })
 
-    const push = confirm("푸시 알림을 받으시겠습니까?")
-
-    if(push){
+    if(push.isConfirmed){
         const notification = await Notification.requestPermission()
         console.log("notification", notification)
     }
