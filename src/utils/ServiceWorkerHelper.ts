b42995ee3920fd7b5d1a4efbaedf831093fb1d9f
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-08-19 16:06:34 +0900

    TEST. 푸시 테스트

--- src/utils/ServiceWorkerHelper.ts
+++ src/utils/ServiceWorkerHelper.ts
@@ -3,6 +3,8 @@ export default class ServiceWorkerHelper {
     static messageChannel: MessageChannel = new MessageChannel()
 
     static async registerServiceWorker() {
+        await navigator.serviceWorker.ready
+
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.addEventListener('message', function(event) {
                 console.log('메인 스레드에서 받은 메시지:', event.data);
@@ -12,29 +14,27 @@ export default class ServiceWorkerHelper {
             if (!registration) {
                 await navigator.serviceWorker.register('../service-worker.js')
             } else {
+
                 console.log('Service Worker is already registered.')
             }
         } else {
             console.log('Service Worker is not supported by browser.');
         }
-        await navigator.serviceWorker.ready
+
+
+        await this.showNotification('hello', { body: 'hello' })
     }
 
 
     static async sendMessageToServiceWorker(message: any) {
         navigator.serviceWorker.controller.postMessage({
-            command: 'fetchData',
-            payload: '데이터 요청'
+            command: 'message',
+            payload: message
         });
     }
 
     static async pushMessageToServiceWorker(message: any) {
-        const registration = await navigator.serviceWorker.getRegistration('../service-worker.js')
-        if (registration) {
-            registration.active?.postMessage(message)
-        } else {
-            console.log('Service Worker is not registered.')
-        }
+
     }
 
     static async showNotification(title: string, options?: NotificationOptions) {
